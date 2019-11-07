8832a97785d633b7d234a38556496fa90331ebc7
jatinnr <56562571+jatinnr@users.noreply.github.com>
2019-11-07 19:33:24 +0530

    Create AddNumber.cpp

---
+++ AddNumber.cpp
@@ -0,0 +1,13 @@
+/* Program to add two numbers */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a=10;
+    int b=15;
+    int c=a+b;
+    
+    cout<<"Sum of a+b is " << c;     // Prints Sum
+}
